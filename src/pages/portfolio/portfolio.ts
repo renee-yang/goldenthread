efc48f1ce6e2fffd36737cb9cc1fad7a5a86768c
reneeyang99 <reneeyang99@gmail.com>
2018-06-01 08:22:55 +0200

    updated chart

--- src/pages/portfolio/portfolio.ts
+++ src/pages/portfolio/portfolio.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from '../../models/user';
 import { HomePage } from '../home/home';
+import { Charity } from '../../models/charity';
+import { Chart } from 'chart.js';
 
 @IonicPage()
 @Component({
@@ -10,20 +12,53 @@ import { HomePage } from '../home/home';
 })
 export class PortfolioPage {
 
-  public users: Array<User> = [];
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+
+  doughnutChart: any;
+
+  //public users: Array<User> = [];
+  public user: User;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    var user1 = new User();
-    user1.firstname = "Renee";
-    user1.lastname = "Yang";
-    user1.profilepicture = "https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg";
-    user1.location = "Maryland";
-    //user1.charities = ['x', 'y'];
+    this.user = new User();
+    this.user.firstname = "Renee";
+    this.user.lastname = "Yang";
+    this.user.profilepicture = "https://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg";
+    this. user.location = "Maryland";
+    this.user.charities = new Array<Charity>(2);
+    
 
   }
 
   ionViewDidLoad() {
-    
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+ 
+            type: 'doughnut',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56",
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
+            }
+ 
+        });
   }
 
   navigateToHome() {
